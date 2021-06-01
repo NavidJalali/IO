fd42b990f1e0276f5306a010380e7f652570a40b
Navid <navid.jalali@prezi.com>
2021-06-01 15:51:58 +0200

    🔧 remove test file

--- fuck.js
+++
@@ -1,9 +0,0 @@
-const IO = require("./lib/index").IO
-const RetryPolicies = require("./lib/index").RetryPolicies
-
-const t = new IO((resolve, reject) => {
-    resolve("in_progress")
-})
-.retry(RetryPolicies.spaced(100, 3))
-.tap(console.log)
-.run()
